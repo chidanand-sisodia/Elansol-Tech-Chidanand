075666d7103671c850816ebedda31f9ff33ee283
Chidanand Sisodia <62592158+chidanand-sisodia@users.noreply.github.com>
2024-04-08 02:10:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,11 +10,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json()); // for parsing application/json
-app.use(cors({
-    origin:["https://elansol-tech-chidanand.vercel.app/"],
-    methods:["POST","GET"],
-    credentials:true
-}))
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI);
 
